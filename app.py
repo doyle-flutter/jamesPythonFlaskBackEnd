34eaa0108f3d405bfe856e2ef2193242ca59c4bf
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-09-07 15:23:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,62 @@
-## With 구름 IDE
-
 from flask import Flask
 from sqlalchemy import create_engine, text
 application = Flask(__name__)
 
-# DB Info
 user = 'root'
 password = '000000'
-database = 'app'
+database = 'fapp'
 host = 'localhost'
 port = 3306
 dbUri = f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}?charset=utf8'
 db = create_engine(dbUri, encoding='utf-8', max_overflow=0)
 
-@application.route("/")
+@application.route("/", methods=['GET'])
 def hello():
     return "Hello goorm!"
 
 @application.route('/dbs')
 def dbs():
-    rows = db.execute(text('SELECT * FROM tableName')).fetchall()
+    rows = db.execute(text('SELECT * FROM fdata')).fetchall()
     print(rows)
     return 'dbs' if not rows else str(rows)
 
 @application.route('/dbs/create/<string:name>')
 def dbinsert(name):
     data = {'data':name}
-    sql = text("INSERT INTO tableName (name) VALUES (:data)")
+    sql = text("INSERT INTO fdata (name) VALUES (:data)")
     rows = db.execute(sql,data).lastrowid
     print(rows)
     return 'complete!' if not rows == 0 else "faile"
 
+from flask import request
+# /qs?q=123
+@application.route("/qs", methods=['GET'])
+def helloQs():
+    argQs = request.args.get('q')
+    print(argQs)
+    return f"Hello {argQs}!"
+
+@application.route('/headers', methods=['GET'])
+def headerPage():
+    ct = request.headers['User-Agent']
+    return f"Hi {str(ct)}"
+
+@application.route('/postbody', methods=['POST'])
+def postBodyLogic():
+    pb = request.form['key']
+    return {'key2':pb}
+
+@application.route('/xpostbody', methods=['POST'])
+def xPostBodyLogic():
+    try:
+        ct = request.headers.get('Content-Type')
+        if(str(ct) == "application/x-www-form-urlencoded"):
+            formData = request.form['key']
+            return f'DATA : {formData} !'
+        else:
+            return "TYPE ERR"
+    except:
+        return "HEADER ERR"
 
 if __name__ == "__main__":
-    application.run(host='127.0.0.1', port=int(80))
+    application.run(host='172.17.0.31', port=int(80))
